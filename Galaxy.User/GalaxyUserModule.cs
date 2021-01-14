fa69433187cfa07f58e0a9f5436229a026403ceb
strayrabbit <xiaohui_stone@qq.com>
2021-01-14 15:46:25 +0800

    add UserService

---
+++ Galaxy.User/GalaxyUserModule.cs
@@ -0,0 +1,11 @@
+﻿using Galaxy.User.Contracts;
+using Volo.Abp.Modularity;
+
+namespace Galaxy.User
+{
+    [DependsOn(typeof(GalaxyUserContractModule))]
+    public class GalaxyUserModule : AbpModule
+    {
+
+    }
+}
---
+++ Galaxy.User/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Galaxy.User.Contracts;
+using Galaxy.User.Contracts.Models.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Galaxy.User
+{
+    public class UserService : ApplicationService, IUserSerivce
+    {
+        public Task<UserDto> GetAsync()
+        {
+            return Task.FromResult(new UserDto()
+            {
+                Id = "1",
+                Name = "name",
+            });
+        }
+    }
+}
